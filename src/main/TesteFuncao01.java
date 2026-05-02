6e7c5300649f5f098dcf5627b1e2ba433a9456fa
Rani <ranieridasilvamendes159@gmail.com>
2026-05-02 15:02:30 -0300

    Initial Commit

--- src/main/TesteFuncao01.java
+++ src/main/TesteFuncao01.java
@@ -1,4 +1,18 @@
+/**Programa que explica os metodos void e return, onde o void não retorna nada e o return retorna um valor, podendo ser armazenado em uma variável.
+ *
+ */
+
 package main;
 
 public class TesteFuncao01 {
+
+    static void soma(int a, int b){
+        int s = a + b;
+        System.out.print("A soma é " + s);
+    }
+
+    static void main() {
+        System.out.println("Começou o programa");
+        soma(5, 2);
+    }
 }
--- src/main/TesteFuncao02.java
+++ src/main/TesteFuncao02.java
@@ -1,4 +1,17 @@
+/**Programa que explica os metodos void e return, onde o void não retorna nada e o return retorna um valor, podendo ser armazenado em uma variável.
+ *
+ */
 package main;
 
 public class TesteFuncao02 {
+    static int soma(int a, int b){
+        int s = a + b;
+        return s;
+    }
+
+    static void main() {
+        System.out.println("Começou o programa");
+        int sm = soma(5, 2);
+        System.out.println("A soma é " + sm);
+    }
 }
